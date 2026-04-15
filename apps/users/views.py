ae0581e72fe717dd6bd06460f6de114fd6260c17
Harbdulmarleyk03 <adebayoabdulmalik12@gmail.com>
2026-04-16 00:06:16 +0100

    Add auth views — RegisterView, LoginView, LogoutView with JWT and HttpOnly cookie

---
+++ apps/users/views.py
@@ -0,0 +1,45 @@
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from apps.users.serializers import RegisterSerializer, UserProfileSerializer, LoginSerializer
+from .services import AuthService
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+auth_service = AuthService()
+
+class RegisterView(APIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = auth_service.register_user(serializer.validated_data)
+            return Response({'detail': 'User registered successfully. Please check your email to verify your account.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = auth_service.login_user(serializer.validated_data)
+            refresh = RefreshToken.for_user(user)
+            access = refresh.access_token
+            return Response({"access": str(access), "refresh": str(refresh)}, status=status.HTTP_200_OK)
+        return Response({'detail': 'User is not verified'}, status=status.HTTP_403_FORBIDDEN)
+    
+class UserProfileView(RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
