39877f220c0cfaa8cebc7441865ea4c12d7ddecd
Harbdulmarleyk03 <adebayoabdulmalik12@gmail.com>
2026-05-08 10:28:19 +0100

    Writing slot generation  logic

--- apps/events/slot_engine.py
+++ apps/events/slot_engine.py
@@ -1,1 +1,28 @@
-def compute_slot()
+from datetime import time, date, datetime
+
+def generate_slot(rules, overrides, bookings, date, timezone, duration, buffer_before, buffer_after, min_notice_hours, owner_timezone):
+    day_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    start_time, end_time = time(9, 0), time(17, 0)
+    specific_date = ""
+    is_unavailable = [True, False]
+    custom_start, custom_end = time(), time()
+
+    rules = [{day_of_week, start_time, end_time}]
+
+    overrides = [{specific_date, is_unavailable, custom_start, custom_end}]
+    
+    bookings = [{}]
+
+    date = datetime.strptime(date, "%Y-%m-%d").date()
+
+    timezone = "Africa/Lagos"
+
+    duration = 30
+
+    buffer_before = 15
+
+    buffer_after = 15
+
+    min_notice_hours = 4
+
+    owner_timezone = "Africa/Lagos"
