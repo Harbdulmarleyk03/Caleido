7227967525dd921982a2ddb3a8bcda81c36ee31a
Harbdulmarleyk03 <adebayoabdulmalik12@gmail.com>
2026-05-25 18:30:42 +0100

    generating and verifying token for cancel booking

---
+++ apps/bookings/tokens.py
@@ -0,0 +1,16 @@
+from django.core import signing
+from apps.bookings.models import Booking
+
+def generate_cancel_token(booking):
+    signed_token = signing.dumps({'booking_id': str(booking.id)}, salt='cancel-booking')
+    return signed_token
+
+def verify_cancel_token(token, max_age=604800):
+    try:
+        data = signing.loads(token, salt='cancel-booking', max_age=max_age)
+        booking = Booking.objects.get(id=data['booking_id'])
+        return booking 
+    except signing.SignatureExpired:
+        raise ValueError("Token Expired")
+    except signing.BadSignature:
+        raise ValueError("Token Tampered") 
