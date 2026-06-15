401ca95e24ceffec9428cb86aff75c2036aed947
Harbdulmarleyk03 <adebayoabdulmalik12@gmail.com>
2026-06-15 11:15:52 +0100

    Conflict and duplicate Error fixed in idempotency in redis implementation

--- apps/bookings/idempotency.py
+++ apps/bookings/idempotency.py
@@ -1,26 +1,31 @@
 from django.core.cache import cache
-from apps.bookings.services import BookingService
 from common.exceptions import ConflictError
+from apps.bookings.models import Booking 
 
-CACHE_TIMEOUT = 300
+IN_PROGRESS_TIMEOUT = 30 
+CACHE_TIMEOUT = 600
 
 def build_booking_cache_key(idempotency_key, user_id):
     return f"idempotency:{user_id}:{idempotency_key}"
 
-def create_booking_in_redis(idempotency_key, user_id):
+def create_booking_in_redis(idempotency_key, user_id, create_booking):
     cache_key = build_booking_cache_key(idempotency_key=idempotency_key, user_id=user_id)
 
-    cached_create = cache.add(cache_key, "IN_PROGRESS", timeout=CACHE_TIMEOUT)
+    cached_create = cache.add(cache_key, "IN_PROGRESS", timeout=IN_PROGRESS_TIMEOUT)
 
     if cached_create is True:
         try:
-            booking = BookingService.create_booking(idempotency_key, user_id)
-            cached_result = cache.set(cache_key, booking.id)
-            return cached_result
+            booking = create_booking()
+            cache.set(cache_key, booking.id, timeout=CACHE_TIMEOUT)
+            return booking
         except Exception:
             cache.delete(cache_key)
-            raise ConflictError("The slot is already taken") 
+            raise 
     
     cached_value = cache.get(cache_key)
 
-    return cached_value
+    if cached_value in ("IN_PROGRESS", None):
+        raise ConflictError("A request with this idempotency key is already being processed.")
+
+    return Booking.objects.get(id=cached_value)
+ 
