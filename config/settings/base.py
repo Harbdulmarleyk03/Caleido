09167d0b737b2ee3e18abece3a67e81e63e332dc
Harbdulmarleyk03 <adebayoabdulmalik12@gmail.com>
2026-06-30 22:45:06 +0100

    refactor: add safe defaults to base.py env vars, simplify CI env block, reformat with black

--- config/settings/base.py
+++ config/settings/base.py
@@ -143,7 +143,9 @@ CACHES = {
     }
 }
 
-EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+EMAIL_BACKEND = env(
+    "EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+)
 EMAIL_HOST = env("EMAIL_HOST", default="localhost")
 EMAIL_PORT = env.int("EMAIL_PORT", default=25)
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
